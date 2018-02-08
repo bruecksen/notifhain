f50a64626c4a6f303985a62e3d56323dc6e11077
Matthias Brück <hi@brueck.io>
2018-02-08 16:05:47 +0100

    remove celery

--- config/settings/common.py
+++ config/settings/common.py
@@ -242,8 +242,8 @@ ADMIN_URL = r'^admin/'
 # ------------------------------------------------------------------------------
 
 # CELERY STUFF
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
